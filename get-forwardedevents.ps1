41bbd4a8c6ddb30c9e4d18e3f9265647cbfe0858
alex-0campo <alexo@landesa.org>
2018-12-30 21:54:08 -0800

    exclude txt files via .gitmore

--- get-forwardedevents.ps1
+++ get-forwardedevents.ps1
@@ -2,11 +2,18 @@
 param()
 
 Clear-Host
-    
-$xpath = "*[System[TimeCreated[timediff(@SystemTime) <= 84600000]]]"
-# $xpath = "*[System[band(keywords,4503599627370496) and TimeCreated[timediff(@SystemTime) <= 84600000]]]"
-$svr = "192.168.56.156"
 
-Invoke-Command -ComputerName $svr -ArgumentList $xpath -ScriptBlock { Get-WinEvent -LogName 'Application' -FilterXPath $args[0] } -UseSSL
+Push-Location
+[string] $path = Split-Path -Parent $PSCommandPath
+cd $path
+    
+$xpath = "*[System[TimeCreated[timediff(@SystemTime) <= 600000]]]"
+$logName = "Security"
+# $xpath = "*[System[band(keywords,4503599627370496) and TimeCreated[timediff(@SystemTime) <= 84600000]]]"
+$svr = "192.168.56.156" # Get-Content .\svr.txt
+
+Invoke-Command -ComputerName $svr -ArgumentList $logName,$xpath -ScriptBlock { Get-WinEvent -LogName $args[0] -FilterXPath $args[1] | ft } # -UseSSL
+
+Pop-location
 
 Write-Host "end of script!`r`n"
